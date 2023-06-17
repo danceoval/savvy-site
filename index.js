f65b50e2a82a71c841be844eb2f947c0b55b64eb
Dan Sohval <daniel.sohval@gmail.com>
2023-06-17 18:48:07 -0400

    node app working

---
+++ index.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const express = require('express');
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Error Handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, '.', 'public/index.html'))
+})
+
+
+
+app.listen(PORT, () =>
+	console.log(`Mixing it up on port ${PORT}`)
+)
